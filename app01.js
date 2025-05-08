56e25148abe68dd0c38436ab5d07491fe48c42e6
Karen Valentim <karenvalentim54@gmail.com>
2025-05-08 19:35:21 -0300

    Tipos de dados

--- app01.js
+++ app01.js
@@ -12,6 +12,19 @@ nome = "Braian"
 idade = "28"
 console.log (nome, idade)
 
+//Tipos de dados
+//text == string
+let cidade = "americana"
+
+//númerico = number
+let salario = 1500.35
+
+//boleano == boolean
+let fumante = false
+console.log(typeof cidade)
+console.log (typeof salario)
+console.log(typeof fumante)
+
 //  Operadores
 //  Operador Atribuição
 //  Operadores aritméticos
